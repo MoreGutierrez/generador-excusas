d20fffdf7b91e4e6f92a49343dfd454a786f18a2
MoreGutierrez <morelstuff2000@gmail.com>
2025-09-13 00:01:10 -0300

    lista excusas con btn mostrar-ocultar

--- GeneradorExcusas/src/App.tsx
+++ GeneradorExcusas/src/App.tsx
@@ -1,11 +1,13 @@
 
 import './App.css'
 import GeneradorExcusas from './components/Generador'
+import ListaExcusas from './components/ListaExcusa'
 
 function App() {
   return (
     <>
       <GeneradorExcusas />
+      <ListaExcusas />
     </>
   )
 }
--- GeneradorExcusas/src/components/ListaExcusa.tsx
+++ GeneradorExcusas/src/components/ListaExcusa.tsx
@@ -1,12 +1,29 @@
-
-
+import { useState } from "react";
+import arrayExcusas from "./../data/arrayExcusas";
 
 function ListaExcusas() {
+  const [mostrarLista, setMostrarLista] = useState(false);
+
+  const mostrarOcultarLista = () => {
+    setMostrarLista(!mostrarLista);
+  };
 
   return (
-    <></>
-  );
+    <>
+      <h2>Lista de Excusas</h2>
+      <button onClick={mostrarOcultarLista}>
+        {mostrarLista ? "Ocultar Lista" : "Mostrar Lista"}
+      </button>
 
+      {mostrarLista && (
+        <ul>
+          {arrayExcusas.map((excusa) => (
+            <li key={excusa.id}>{excusa.texto}</li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
 
 export default ListaExcusas;
